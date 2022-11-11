b204ac0783877a7d197fcd2310dedc4c732610e7
Mark <markkolenov13@gmail.com>
2022-11-11 14:16:01 +0200

    HW1

---
+++ myapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, re_path
+from .views import articles, articles_archive, \
+    users, articles_num, articles_archive_num, \
+    users_num, regular, number_phone, first
+
+urlpatterns = [
+    path('', first),
+    path('articles/', articles),
+    path('articles/archive/', articles_archive),
+    path('users/', users),
+    path('articles/<int:article_number>', articles_num),
+    path('articles/<int:article_number>/<slug:slug_text>', articles_archive_num),
+    path('users/<int:article_number>', users_num),
+    re_path('[1-9a-f]{4}-[1-9a-f]{6}', regular),
+    re_path('articles/[+][0-9]{10,14}', number_phone),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+def first(request):
+    return HttpResponse("Hi")
+
+def articles(request):
+    return HttpResponse("http://127.0.0.1:8000/articles")
+
+def articles_archive(request):
+    return HttpResponse("http://127.0.0.1:8000/articles/archive")
+
+def users(request):
+    return HttpResponse("http://127.0.0.1:8000/users")
+
+def articles_num(request, article_number):
+    return HttpResponse("articles/<int:article_number>")
+
+def articles_archive_num(request, article_number, slug_text):
+    return HttpResponse("articles/<int:article_number>/<slug:slug_text>")
+
+def users_num(request, article_number):
+    return HttpResponse("http://127.0.0.1:8000/users/int:user_number>")
+
+def regular(request):
+    return HttpResponse("Regular Viragenie Good")
+
+def number_phone(request):
+    return HttpResponse('number good')
