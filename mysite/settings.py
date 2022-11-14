6af009d48d53aa56a0c231948fb717d7aa07f9a9
Walrusan <55920163+Walrus15@users.noreply.github.com>
2022-11-14 15:24:28 +0200

    Revert "Hw3"

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database',
-        'USER': 'walrusan',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
