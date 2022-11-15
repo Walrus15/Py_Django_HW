4c5654e3ddd24954d5e31c100904003eafed1293
Mark <markkolenov13@gmail.com>
2022-11-15 12:07:02 +0200

    Models2

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+# 1. Каталог
 class Author(models.Model):
     name = models.CharField(max_length=59)
 
@@ -8,16 +10,15 @@ class Book(models.Model):
     date = models.DateField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-
-
-class Author_two(models.Model):
+# 2. Библиотека
+class AuthorTwo(models.Model):
     name = models.CharField(max_length=59)
     surname = models.CharField(max_length=59)
 
     def __str__(self):
         return self.name
 
-class User_two(models.Model):
+class UserTwo(models.Model):
     name = models.CharField(max_length=59)
     surname = models.CharField(max_length=59)
     time_take = models.DateTimeField(auto_now_add=True)
@@ -25,33 +26,48 @@ class User_two(models.Model):
     def __str__(self):
         return self.name
 
-class Book_two(models.Model):
+class BookTwo(models.Model):
     title = models.CharField(max_length=119)
     date = models.DateField(blank=True)
-    author = models.ManyToManyField(Author_two, blank=True)
-    user = models.ManyToManyField(User_two, blank=True)
-    availability = models.BooleanField(blank=True, default=True)
+    author = models.ManyToManyField(AuthorTwo, blank=True)
+    user = models.ForeignKey(UserTwo, blank=True)
 
     def __str__(self):
         return self.title
 
+class Availability(models.Model):
+    user = models.ForeignKey(UserTwo, on_delete=models.CASCADE)
+    book = models.ForeignKey(BookTwo, on_delete=models.CASCADE)
 
-class Likes(models.Model):
-    like = models.BooleanField()
+# 3. Статья
+class UserThree(models.Model):
+    name = models.CharField(max_length=59)
+    surname = models.CharField(max_length=59)
 
-class Comments(models.Model):
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    user = models.ForeignKey(UserThree, on_delete=models.CASCADE)
     comment = models.TextField(max_length=359)
-    like = models.OneToOneField(Likes, on_delete=models.CASCADE, blank=True)
-    com_com = models.OneToOneField('myapp.Comments', on_delete=models.CASCADE, blank=True)
+    com_com = models.ForeignKey('myapp.Comments', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return self.comment
 
-class Articles(models.Model):
+class Article(models.Model):
+    user = models.ForeignKey(UserThree, on_delete=models.CASCADE)
     title = models.CharField(max_length=59)
     description = models.TextField(max_length=359)
-    comment = models.OneToOneField(Comments, on_delete=models.CASCADE, blank=True)
-    like = models.OneToOneField(Likes, on_delete=models.CASCADE, blank=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return self.title
+
+class LikeComment(models.Model):
+    user = models.ForeignKey(UserThree, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+class LikeArticle(models.Model):
+    user = models.ForeignKey(UserThree, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
